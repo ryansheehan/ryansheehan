8da24f46720e3a24970769b86df3f653f810e822
ryan <rsheehan@gmail.com>
2022-01-11 12:44:34 -0600

    setup postcss and static file output

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,41 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+import autoprefixer from 'autoprefixer';
+import presetEnv from 'postcss-preset-env';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({
+    sourceMap: true,
+    postcss: {
+      plugins: [
+        autoprefixer(),
+        presetEnv({
+          features: {
+            'nesting-rules': true,
+            'custom-properties': true,
+            'gap-properties': true,
+            'not-pseudo-class': true,
+            'custom-media-queries': true,
+          },
+        }),
+      ],
+    },
+  }),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+      base: dev ? '' : '',
+    }),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+
+    appDir: 'internal',
 	}
 };
 
