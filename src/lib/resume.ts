d756f601038b025ca890a6aac0a86923e5938c3e
ryan <rsheehan@gmail.com>
2022-01-17 22:15:05 -0600

    edited grammer and some content in experiences

--- src/lib/resume.ts
+++ src/lib/resume.ts
@@ -103,7 +103,7 @@ export const resume: Resume = {
       start: 'Mar 2015',
       end: 'Nov 2019',
       achievements: [
-        `Build and maintained a project starter used by developers across Tyler to kickstart their web applications`,
+        `Built and maintained a project starter used by developers across Tyler to kickstart their web applications`,
         `Served as an R&D developer prototyping application ideas and 3rd party tools`,
         `Developed and promoted the use of docker within Tyler`,
         `Helped to administer TeamCity, ProGet, and Bitbucket`,
@@ -116,7 +116,7 @@ export const resume: Resume = {
       start: 'Jun 2013',
       end: 'Mar 2015',
       achievements: [
-        `Lead a cross functional team of 11 people in agile practices as a scrum master`,
+        `Led a cross functional team of 11 people in agile practices as a scrum master`,
         `Developed an automated pipeline for acceptance testing from Gherkins to C#`,
         `Coached and gave presentations to external teams on how to execute proper agile practices`,
         `Developed a Jury application utilizing WPF with extensibility through MEF`,
@@ -129,7 +129,7 @@ export const resume: Resume = {
       end: 'Jun 2013',
       achievements: [
         `Lead developer on a large WPF application design to manage and track down-hole drilling tools`,
-        `Developed a near-realtime 3d application, with Unity3D, that reflected down-hole tool data`,
+        `Developed a near-realtime 3D application, with Unity3D, that reflected down-hole tool data`,
         `Oversaw and reviewed code developed by an outsourced team located in Vietnam`,
         `Received extensive training in agile development`
       ]
@@ -151,10 +151,9 @@ export const resume: Resume = {
       title: 'Co-owner and developer',
       start: 'Sept 2020',
       achievements: [
-        `Food blog and recipe site with an emphasis on automating nutritional calculation.`,
-        `Caters to people that have a medical need for accurate nutrition`,
-        `Under active development with a plan to release in 2022.`,
-        `Built with typescript, node, svelte, sveltekit, groq, and sanity.io`,
+        `Sole developer building a food and recipe blog with an emphasis on dynamically calculating nutritional data.`,        
+        `Building with typescript, node, svelte, sveltekit, groq, and sanity.io as a low-code headless cms`,
+        `Manage business needs, affiliate relationships, and marketing tools`,
       ]
     },
     {
@@ -163,9 +162,9 @@ export const resume: Resume = {
       start: 'Sept 2009',
       end: 'Oct 2014',
       achievements: [
-        `Online artificial wedding bouquets, boutonnieres, and hair-flowers`,
+        `Built online store front for artificial wedding bouquets, boutonnieres, and hair-flowers`,
         `Developed the store front with Magento`,
-        `Helped process orders, general accounting, managed part-time staff`,
+        `Managed part-time staff, general accounting, and help process orders`,
       ]
     }
   ],
