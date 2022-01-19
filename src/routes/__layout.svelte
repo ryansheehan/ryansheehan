02fa2b1fb500e55aa41211d4525e7a2951c49e41
ryan <rsheehan@gmail.com>
2022-01-19 14:35:25 -0600

    changed header font

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -12,8 +12,8 @@
 
 <svelte:head>
   <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
-  <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=Signika+Negative:wght@400;700&display=swap" rel="stylesheet">   
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">  
+  <link href="https://fonts.googleapis.com/css2?family=PT+Serif&family=Signika+Negative:wght@400;700&display=swap" rel="stylesheet">
 </svelte:head>
 
 
