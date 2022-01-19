f877308e565e75968468cad6c34fcdaed2696872
ryan <rsheehan@gmail.com>
2022-01-19 16:47:20 -0600

    added og meta and image

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,10 +6,22 @@
 
   import { resume } from '$lib/resume';
   const { summary, skills, experience, education } = resume;
+
+  const host = 'https://ryansheehan.github.io'
+  const ogImageUrl = `${host}/images/do-something-great-1200x627.jpg`
 </script>
 
 <svelte:head>
-  <title>Ryan Sheehan resume</title>
+  <title>Ryan Sheehan Resume</title>
+  <meta property="og:site_name" content="Ryan Sheehan" />
+  <meta property="og:title" content="Software Engineer &amp; Development Manager"/>
+  <meta property="og:description" content="Ryan Sheehan is a Software Engineer and Development Manager. Come check out what I'm up to..."/>
+  <meta property="og:url" content="{host}"/>
+  <meta property="og:type" content="website"/>
+  <meta property="og:image" content="{ogImageUrl}"/>
+  <meta property="og:image:secure_url" content="{ogImageUrl}"/>
+  <meta property="og:image:width" content="1200"/>
+  <meta property="og:image:height" content = "627"/>
 </svelte:head>
 
 <div>
