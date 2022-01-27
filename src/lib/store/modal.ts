3f37f0f8e6225c423e177b9c0329585109c07236
ryan <rsheehan@gmail.com>
2022-01-26 20:16:21 -0600

    restore modal ts, removed toggle option

--- src/lib/store/modal.ts
+++ src/lib/store/modal.ts
@@ -1,21 +1,18 @@
 import { writable } from 'svelte/store';
 import type {Readable} from 'svelte/store';
 
-export interface Modal extends Readable<boolean> {
-  toggle: () => void;
+export interface Modal extends Readable<boolean> {  
   open: () => void;
   close: () => void;
 }
 
 export function createModal(): Modal {
-  const {subscribe, update, set} = writable(false);
+  const {subscribe, set} = writable(false);
   return {
     subscribe,
-    toggle: () => update(v => !v),
     open: () => set(true),
     close: () => set(false),
   }
 }
 
 export const emailModal = createModal();
-
