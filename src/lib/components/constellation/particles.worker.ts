6016bebfa316e73030bc8233150beaf606887046
Ryan Sheehan <rsheehan@gmail.com>
2024-07-29 17:35:01 -0500

    increased friction

--- src/lib/components/constellation/particles.worker.ts
+++ src/lib/components/constellation/particles.worker.ts
@@ -29,6 +29,26 @@ addEventListener('message', ({data}: MessageEvent<ParticleWorkerData>) => {
     const particlesView = new Float32Array(particlesBuffer, particleOffset * byteStride, particleCount * stride);
     const renderingView = new Uint8Array(renderingBuffers[backBuffer]);
 
+    // formula for graviation force between two particles
+    // F = G * m1 * m2 / r^2
+    // where G is the gravitational constant
+    // m1 and m2 are the massses of the gravity well and particle
+    // r is the distance between the gravity well and the particle
+    // 
+    // if we assume G=1 and m1=1 then the formula simplifies to
+    // F = m2 / r^2
+    // assumes gravity = 1 and particle mass = 1
+    const gravityWellMass = 516600; // 600000; //258300 * 2 = 516600
+    
+    // if the gravity well is turned on, then attract everything
+    // this is just for fun!
+    const minForce = 2400;
+
+    // how the the particles slow down when no force is applied
+    // the closer this is to 1 the less friction and the more particles
+    // will slide without the presence of the gravity well
+    const frictionScalar = 0.8; // 0.75;
+
     let x, xi, y, yi, vx, vxi, vy, vyi, gravDx, gravDy, dist, force: number;
 
     // update particles
@@ -43,13 +63,13 @@ addEventListener('message', ({data}: MessageEvent<ParticleWorkerData>) => {
         vx = particlesView[vxi];
         vy = particlesView[vyi];
 
-        gravDx = vx * 0.9;
-        gravDy = vy * 0.9;
+        gravDx = vx * frictionScalar;
+        gravDy = vy * frictionScalar;
         if(pointerDown) {
             gravDx = pointerX - x;
             gravDy = pointerY - y;
             dist = Math.sqrt(gravDx * gravDx + gravDy * gravDy);
-            force = 2 * Math.min(1200, 258300 / (dist * dist));
+            force = Math.min(minForce, gravityWellMass / (dist * dist));
             gravDx *= force / dist;
             gravDy *= force / dist;
         }
